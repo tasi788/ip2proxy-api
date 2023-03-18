a20a3a0d6819c4ac06c4a25f9f32bb4a8349cf18
tasi788 <tdc@tdccc.com.tw>
2023-03-18 14:09:37 +0800

    👌 enum case

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use ip2location::{error, Record, DB};
+
+const FILES: &str = "src/ip2proxy.BIN";
+
 fn main() {
-    println!("Hello, world!");
+    ip_lookup_in_ipv4bin().unwrap();
+    // println!("Hello, world!");
+}
+
+fn ip_lookup_in_ipv4bin() -> Result<(), error::Error> {
+    let mut db = DB::from_file(FILES)?;
+    let record = db.ip_lookup("104.16.0.132".parse().unwrap())?;
+    match record {
+        Record::ProxyDb(proxy) => println!("{:?}", proxy),
+        _ => println!("No record found"),
+    }
+    Ok(())
 }
